76effbb57bf8052cdee6793ca16e02ec67dd66e8
Rajesh Kommu <rkkommu@ucdavis.edu>
2020-10-26 17:20:38 -0700

    Finished Filtering (video 9) video code

--- live_view_studio/lib/live_view_studio_web/router.ex
+++ live_view_studio/lib/live_view_studio_web/router.ex
@@ -24,6 +24,7 @@ defmodule LiveViewStudioWeb.Router do
     live "/search", SearchLive
     live "/autocomplete", AutocompleteLive
     live "/flights", FlightsLive
+    live "/filter", FilterLive
   end
 
   # Other scopes may use custom stacks.
