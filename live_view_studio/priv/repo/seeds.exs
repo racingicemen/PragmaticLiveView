89963665b9eb01c908923bb4534541a2d90b97a8
Rajesh Kommu <rkkommu@ucdavis.edu>
2020-11-22 21:15:38 -0800

    Completed exercise code of pagination (video 11)

--- live_view_studio/priv/repo/seeds.exs
+++ live_view_studio/priv/repo/seeds.exs
@@ -14,6 +14,7 @@ alias LiveViewStudio.Repo
 alias LiveViewStudio.Boats.Boat
 alias LiveViewStudio.Servers.Server
 alias LiveViewStudio.Donations.Donation
+alias LiveViewStudio.Vehicles.Vehicle
 
 %Boat{
   model: "1760 Retriever Jon Deluxe",
@@ -270,6 +271,15 @@ for _i <- 1..100 do
     item: item,
     quantity: Enum.random(1..20),
     days_until_expires: Enum.random(1..30)
+  }
+  |> Repo.insert!()
+end
+
+for _i <- 1..1000 do
+  %Vehicle{
+    make: Faker.Vehicle.make(),
+    model: Faker.Vehicle.model(),
+    color: Faker.Color.name()
   }
   |> Repo.insert!()
 end
