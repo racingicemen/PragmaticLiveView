808201914d3de9b220b4304b6166eb6c6085e141
Rajesh Kommu <rkkommu@ucdavis.edu>
2020-12-17 21:39:30 -0800

    Add a note for where the name of the key in the params map comes from

--- live_view_studio/lib/live_view_studio_web/live/volunteers.ex
+++ live_view_studio/lib/live_view_studio_web/live/volunteers.ex
@@ -18,6 +18,11 @@ defmodule LiveViewStudioWeb.VolunteersLive do
     {:ok, socket, temporary_assigns: [volunteers: []]}
   end
 
+  # The "volunteer" in %{"volunteer" => params} in the second argument to handle_event
+  # should match the (lowercased) name of the of the schema module defined in
+  # <...>/live_view_studio/lib/live_view_studio/volunteers/volunteer.ex
+  # defmodule LiveViewStudio.Volunteers.Volunteer would require %{"volunteer" => params}
+  # defmodule LiveViewStudio.Volunteers.Volumteer would require %{"volumteer" => params}
   def handle_event("save", %{"volunteer" => params}, socket) do
     case Volunteers.create_volunteer(params) do
       {:ok, volunteer} ->
